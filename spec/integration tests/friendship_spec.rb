38312993c6f8a496aa901814e7747b8645178a9a
Omar Labana <omarlabana@outlook.com>
2021-07-18 08:39:45 +0200

    Add some integration tests

---
+++ spec/integration tests/friendship_spec.rb
@@ -0,0 +1,65 @@
+require 'rails_helper'
+
+RSpec.describe 'friendship model associations', type: :system do
+  before(:each) do
+    User.create(name: 'omar',
+                email: 'omar@mero.com',
+                gravatar_url: '123456789',
+                password: '123456789', password_confirmation: '123456789')
+
+    User.create(name: 'ramo',
+                email: 'ramo@mero.com',
+                gravatar_url: '123456789',
+                password: '123456789', password_confirmation: '123456789')
+
+  end
+
+  it 'adds a user to your friend list' do
+    visit "#{root_path}users/#{User.second.id}"
+    fill_in 'Email', with: 'omar@mero.com'
+    fill_in 'Password', with: '123456789'
+    click_button 'Log in'
+    expect(page).to have_content('Invite to friendship')
+  end
+
+  it 'removes a user from your friend list' do
+    Friendship.create(inviter_id: User.first.id, invitee_id: User.second.id, accepted: true)
+    visit "#{root_path}users/#{User.second.id}"
+    fill_in 'Email', with: 'omar@mero.com'
+    fill_in 'Password', with: '123456789'
+    click_button 'Log in'
+    expect(page).to have_content('End friendship')
+  end
+
+  it 'shows a call to action button when no friendship' do
+    Friendship.create(inviter_id: User.first.id, invitee_id: User.second.id, accepted: true)
+    visit "#{root_path}users/#{User.second.id}"
+    fill_in 'Email', with: 'omar@mero.com'
+    fill_in 'Password', with: '123456789'
+    click_button 'Log in'
+    visit "#{root_path}users/#{User.second.id}"
+    click_link 'End friendship'
+    expect(page).to have_content('Invite to friendship')
+  end
+
+  it 'accepts an invitation' do
+    Friendship.create(inviter_id: User.second.id, invitee_id: User.first.id, accepted: false)
+    visit "#{root_path}users/#{User.second.id}"
+    fill_in 'Email', with: 'omar@mero.com'
+    fill_in 'Password', with: '123456789'
+    click_button 'Log in'
+    expect(page).to have_content('Accept')
+  end
+  
+  # same as test 2
+  it 'end friendship' do
+    Friendship.create(inviter_id: User.second.id, invitee_id: User.first.id, accepted: false)
+    visit "#{root_path}users/#{User.second.id}"
+    fill_in 'Email', with: 'omar@mero.com'
+    fill_in 'Password', with: '123456789'
+    click_button 'Log in'
+    click_link 'Accept'
+    expect(page).to have_content('End friendship')
+  end
+
+end
