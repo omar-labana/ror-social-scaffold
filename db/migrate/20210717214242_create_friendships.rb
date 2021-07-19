df800e32b3bff1247ca54ce67596af02046cb86b
Omar Labana <omarlabana@outlook.com>
2021-07-19 12:36:56 +0200

    Edit friendship migrations

--- db/migrate/20210717214242_create_friendships.rb
+++ db/migrate/20210717214242_create_friendships.rb
@@ -1,9 +1,9 @@
 class CreateFriendships < ActiveRecord::Migration[5.2]
   def change
     create_table :friendships do |t|
-      t.references :inviter, foreign_key: {to_table: :users}
-      t.references :invitee, foreign_key: {to_table: :users}
-      t.boolean :accepted
+      t.references :user, foreign_key: true
+      t.references :friend, foreign_key: { to_table: :users }
+      t.boolean :status
 
       t.timestamps
     end
