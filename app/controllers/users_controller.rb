b8dee91e866ce946e67c4b6e0eb5d7d48d5be577
Omar Labana <omarlabana@outlook.com>
2021-07-21 21:10:40 +0200

    Edit show method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,9 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.ordered_by_most_recent
-    @friends = current_user.friends
-    @pending_inviter_friends = current_user.pending_inviter_friends
-    @pending_invitee_friends = current_user.pending_invitee_friends
+    @pending_inviters = current_user.pending_inviters
+    @pending_invitees = current_user.pending_invitees
+    # TODO => Fix layout + pivot
+    @accepted_friends = current_user.accepted_friends
   end
 end
